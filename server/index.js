df667012ee13f58138c1a7acf003a0889d948dc2
TheJhoxX <victorjorgesibaja@gmail.com>
2023-10-01 00:27:40 +0200

    Configuración de puerto

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors"); // Importa el paquete cors
 const controladorUsuarios = require("./contoladores/user.controller");
 const controladorTareas = require("./contoladores/tareas.controller")
 
-const PORT = 3001;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(
