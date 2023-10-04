a8dcbaf3bd35a6c82ab46bb35a50eaa75b3cffcc
TheJhoxX <victorjorgesibaja@gmail.com>
2023-10-04 16:05:21 +0200

    Cambio cors

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://todo-app-thejhoxx.vercel.app/'],
+    origin: 'https://todo-app-thejhoxx.vercel.app',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Especifica los métodos permitidos
     allowedHeaders: 'Content-Type,Authorization', // Especifica los encabezados permitidos
